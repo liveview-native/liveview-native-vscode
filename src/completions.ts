106f0345dda6b9f9e3ff3d960596b38ae686223f
Carson Katri <Carson.katri@gmail.com>
2023-05-25 09:04:53 -0400

    Add support for modifier completions

--- src/completions.ts
+++ src/completions.ts
@@ -1,10 +1,65 @@
 import * as vscode from 'vscode';
 
-import * as path from 'path';
-import * as fs from 'fs';
-
 import { getDocs, getViews } from './documentation';
 import * as markdown from './markdown';
+import { execSync } from 'child_process';
+
+const modifierListScript = `
+defmodule Info do
+  def cast_type({:parameterized, _, %{ :mappings => mappings }}), do: Map.keys(Enum.into(mappings, %{}))
+  def cast_type({:array, type}), do: "[#{type}]"
+  def cast_type(type), do: type
+end
+
+LiveViewNativePlatform.context(%LiveViewNativeSwiftUi.Platform{}).platform_modifiers
+  |> Enum.map(fn {name, mod} -> {
+    name,
+    %{
+      fields: Enum.map(
+        mod.__schema__(:fields),
+        fn field -> %{
+          source: mod.__schema__(:field_source, field),
+          type: Info.cast_type(mod.__schema__(:type, field))
+        } end
+      ),
+    }
+  } end)
+  |> Enum.into(%{})
+  |> Jason.encode!(pretty: true)
+  |> IO.puts
+`;
+
+type ModifierSchema = {
+    fields: {
+        source: string,
+        type: string | string[]
+    }[]
+};
+
+const loadModifiers: () => { [name: string]: ModifierSchema } = () => JSON.parse(
+    execSync(`mix run -e '${modifierListScript}'`, {
+        cwd: vscode.workspace.getWorkspaceFolder(vscode.workspace.workspaceFolders![0].uri)?.uri.path
+    })
+        .toString()
+);
+
+const modifierSnippet = (name: string, modifier: ModifierSchema) => {
+    let snippet = `${name}(`;
+    for (const [i, field] of modifier.fields.entries()) {
+        if (i > 0) {
+            snippet += ', ';
+        }
+        if (field.type instanceof Array) {
+            snippet += `${field.source}: \$\{${i + 1}|${field.type.map((t) => ":" + t).join(",")}|\}`;
+        } else {
+            snippet += `${field.source}: \$\{${i + 1}:${field.type.split('.').at(-1)}\}`;
+        }
+    }
+    snippet += ')';
+    return new vscode.SnippetString(snippet);
+};
+
+var modifiers: { [name: string]: ModifierSchema } | undefined;
 
 const completionItemProvider: vscode.CompletionItemProvider = {
     async provideCompletionItems(document, position, token, context) {
@@ -18,6 +73,7 @@ const completionItemProvider: vscode.CompletionItemProvider = {
                 completion.sortText = `<${view}`;
                 return completion;
             });
+        
         const attrExpr = /\s*<(\w+)\s*((\w|-)+=\"[^\"]*\"\s*)*\w*/;
         const prefix = document.getText(new vscode.Range(position.with({ character: 0 }), position)).match(attrExpr);
         let attributeCompletions: vscode.CompletionItem[] = [];
@@ -33,7 +89,31 @@ const completionItemProvider: vscode.CompletionItemProvider = {
             modifierAttributeCompletion.insertText = new vscode.SnippetString("modifiers={@native |> $0}");
             attributeCompletions.push(modifierAttributeCompletion);
         }
-        return viewCompletions.concat(attributeCompletions);
+
+        if (!modifiers) {
+            try {
+                modifiers = loadModifiers();
+            } catch {
+                modifiers = undefined;
+            }
+        }
+        const modifierCompletions = Object.entries(modifiers ?? {}).map(([name, modifier]) => {
+            const item = new vscode.CompletionItem(
+                {
+                    label: name,
+                    detail: `(${modifier.fields.map(f => f.source).join(', ')})`
+                },
+                vscode.CompletionItemKind.Method
+            );
+            item.insertText = modifierSnippet(name, modifier);
+            return item;
+        });
+
+        return [
+            ...viewCompletions,
+            ...attributeCompletions,
+            ...modifierCompletions
+        ];
     },
 };
 
