d146b2a1296566b38e043b76f4e0ba2fd033d76f
Carson Katri <Carson.katri@gmail.com>
2024-01-30 09:12:04 -0500

    Add more completions for ShapeStyle types

--- src/modifiers.ts
+++ src/modifiers.ts
@@ -34,8 +34,31 @@ export const modifierSnippet = (name: string, signature: ModifierParameter[]) =>
     return new vscode.SnippetString(snippet);
 };
 
+const colors = [
+    "Color(.sRGB, red: 1, green: 1, blue: 1, opacity: 1)",
+    "Color(.sRGB, white: 1, opacity: 1)",
+    ".red",
+    ".orange",
+    ".yellow",
+    ".green",
+    ".mint",
+    ".teal",
+    ".cyan",
+    ".blue",
+    ".indigo",
+    ".purple",
+    ".pink",
+    ".brown",
+    ".white",
+    ".gray",
+    ".black",
+    ".clear",
+    ".primary",
+    ".secondary",
+].map(c => c.split(',').join('\\,').split('|').join('\\|'));
+
 const typeCompletion = (name: string, type: string, index: number) => {
-    const generic = type.match(/^(?<wrapper>\w+)<(?<inner>.+)>$/);
+    const generic = type.match(/^(?<wrapper>\w+)<(?<inner>.+)(?<innerOptional>\??)>(?<wrapperOptional>\??)$/);
     const baseTypeName = (generic?.groups?.inner ?? type).split('.').at(-1)!;
     const cases = modifiers.enums?.[baseTypeName]?.map(c => `.${c}`);
     if (!!generic?.groups) {
@@ -43,21 +66,33 @@ const typeCompletion = (name: string, type: string, index: number) => {
         case 'ChangeTracked':
             return `attr("\$\{${index}:${name}\}")`;
         case 'AttributeReference':
-            return `\$\{${index}|attr("${name}"),${cases?.join(',') ?? type}|\}`;
+            return `\$\{${index}|attr("${name}"),${cases && cases.length > 0 ? cases.join(',') : type}|\}`;
         }
     }
     switch (type) {
-    case 'ViewReference':
-    case 'InlineViewReference':
-    case 'TextReference':
-    case 'ToolbarContentReference':
-    case 'CustomizableToolbarContentReference':
-        return `:\$\{${index}:${name}\}`;
-    case 'Event':
-        return `event("\$\{${index}:${name}\}")`;
-    case 'SwiftUI.LocalizedStringKey':
-    case 'Swift.String':
-        return `"\$\{${index}:${name}\}"`;
+        case 'ViewReference':
+        case 'InlineViewReference':
+        case 'TextReference':
+        case 'ToolbarContentReference':
+        case 'CustomizableToolbarContentReference':
+            return `:\$\{${index}:${name}\}`;
+        case 'Event':
+            return `event("\$\{${index}:${name}\}")`;
+        case 'SwiftUI.LocalizedStringKey':
+        case 'Swift.String':
+            return `"\$\{${index}:${name}\}"`;
+        case 'SwiftUI.Color':
+            return `\$\{${index}|${colors.join(',')}|\}`;
+        case 'AnyShapeStyle':
+            const cases = colors.concat([
+                `.angularGradient(colors: [.blue, .green], center: .center, startAngle: .zero, endAngle: .degrees(180))`,
+                `.conicGradient(colors: [.blue, .green], center: .center)`,
+                `.ellipticalGradient(colors: [.blue, .green])`,
+                `.linearGradient(colors: [.blue, .green], startPoint: .leading, endPoint: .trailing)`,
+                `.radialGradient(colors: [.blue, .green], center: .center, startRadius: 0, endRadius: 10)`,
+                `.image(Image("name"))`,
+            ].map(c => c.split(',').join('\\,').split('|').join('\\|')));
+            return `\$\{${index}|${cases.join(',')}|\}`;
     }
     if (!!cases && cases.length > 0) {
         return `\$\{${index}|${cases.join(',')}|\}`;
